9c41cada0de3994055a6361033a3020e1cce2db3
Ritik kumar <ritikkumarkashyap9@gmail.com>
2024-03-02 00:23:04 +0530

    hii

--- src/Components/FingerPrint/FingerPrint.jsx
+++ src/Components/FingerPrint/FingerPrint.jsx
@@ -65,7 +65,7 @@ const Box = () => {
     <div className="m-4 text-center p-6 box-border shadow-lg flex flex-col items-center   border-1">
       <img className="w-24 h-24 object-cover" src={finger} alt="Box" />
       <button
-      
+        className="mt-4 px-8 py-1.5 border border-blue-500 text-blue-500 rounded-md transition duration-300 hover:bg-blue-500 hover:text-white"
         type="button"
         onClick={captureFingerAndUpload}
         disabled={captureCount >= 5}
