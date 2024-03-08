8bc3c5060537eb5527efdef0b9fcc487e86fc751
DESKTOP-VFB5TE0\Gyanendra Yadav <gyanendrayadav12345789@gmail.com>
2024-03-08 13:25:18 +0530

    protected route is refectore

--- src/Route/Protected.jsx
+++ src/Route/Protected.jsx
@@ -5,70 +5,54 @@ function Protected() {
   const userData = JSON.parse(localStorage.getItem("user"));
   const userRole = userData ? userData.User_type : null;
 
-  // Define routes that are allowed for the "Retailer" role
-  const allowedRoutesForRetailer = [
-    "/retailer",
-    "/add-customer",
-    "/list",
-    "/new-entry",
-    "/child-entry-list",
-    "/mobileupdate",
-    "/mobiledata",
-     
-  ];
-
-  // Define routes that are allowed for the "Admin" and "SuperAdmin" roles
-  const allowedRoutesForAdmin = [
-    "/superadmin",
-    "/adduser",
-    "/viewuser",
-    "/balance_transfer",
-    "/user_limit",
-    "/history",
-    "/add-customer",
-    "/list",
-    "/new-entry",
-    "/child-entry-list",
-    "/mobileupdate",
-    "/mobiledata",
-    "/adminreport",
-    "/viewretaileruserlist",
-    "/searchentrydata",
-    "/ViewChildData",
-    "/editstatus",
-  ];
-  const allowedRoutesForBackoffice=[
-    "/backoffice"
-  ]
+  // Define routes allowed for different roles
+  const allowedRoutes = {
+    Retailer: [
+      "/retailer",
+      "/add-customer",
+      "/list",
+      "/new-entry",
+      "/child-entry-list",
+      "/mobileupdate",
+      "/mobiledata",
+    ],
+    Superadmin: [
+      "/superadmin",
+      "/adduser",
+      "/viewuser",
+      "/balance_transfer",
+      "/user_limit",
+      "/history",
+      "/add-customer",
+      "/list",
+      "/new-entry",
+      "/child-entry-list",
+      "/mobileupdate",
+      "/mobiledata",
+      "/adminreport",
+      "/viewretaileruserlist",
+      "/searchentrydata",
+      "/ViewChildData",
+      "/editstatus",
+    ],
+    BackOffice: ["/backoffice"],
+  };
 
   // Check if the user is logged in
   if (!userData) {
     return <Navigate to="/" />;
   }
 
-  // Check if the user is a "Retailer"
-  if (userRole === "Retailer") {
-    const currentRoute = window.location.pathname;
-    // If the current route is not allowed for the retailer, redirect to home page
-    if (!allowedRoutesForRetailer.includes(currentRoute)) {
-      return <Navigate to="/retailer" />;
-    }
-  }
+  // Check if the current route is allowed for the user's role
+  const allowedRoutesForUserRole = allowedRoutes[userRole];
+  const currentRoute = window.location.pathname;
 
-  // Check if the user is an "Admin" or "SuperAdmin"
-  if (userRole === "Superadmin") {
-    const currentRoute = window.location.pathname;
-    // If the current route is not allowed for the admin, redirect to home page
-    if (!allowedRoutesForAdmin.includes(currentRoute)) {
-      return <Navigate to="/superadmin" />;
-    }
-  }
-  if (userRole === "BackOffice") {
-    const currentRoute = window.location.pathname;
-    // If the current route is not allowed for the admin, redirect to home page
-    if (!allowedRoutesForBackoffice.includes(currentRoute)) {
-      return <Navigate to="/backoffice" />;
-    }
+  if (
+    !allowedRoutesForUserRole ||
+    !allowedRoutesForUserRole.includes(currentRoute)
+  ) {
+    // Redirect to home page for invalid routes
+    return <Navigate to={`/${userRole.toLowerCase()}`} />;
   }
 
   // Render the protected routes
