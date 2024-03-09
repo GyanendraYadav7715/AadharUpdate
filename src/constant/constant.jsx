b43cafb2bccda23e6bcf5f74f204b219602cace6
shashi <sd584911@gmail.com>
2024-03-09 23:31:27 +0530

    sequre

--- src/constant/constant.jsx
+++ src/constant/constant.jsx
@@ -1,5 +1,5 @@
 //Local Url
-export const Local_Url = "http://localhost:5000";
+export const Local_Url = "https://shashi.evinsight.xyz";
 
 //Development Url
 //export const Local_Url = "http://65.0.125.57:5000";
