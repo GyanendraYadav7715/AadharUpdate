4d6efd6e75047085d4d1381189e8a0ae76ea0ea1
Ritik kumar <ritikkumarkashyap9@gmail.com>
2024-03-28 02:15:32 +0530

    f

--- src/Components/Slip/Slip.jsx
+++ src/Components/Slip/Slip.jsx
@@ -2,7 +2,34 @@ import React from "react";
 
 import "./Slip.css";
 
-function Slip({ fileUrl }) {
+function Slip({ fileUrl })
+
+
+{
+
+  const handleClick = async () => {
+    try {
+      // Extract the ID from mobileUserData
+      const { _id: id } = mobileUserData;
+
+      // Make a GET request using Axios to fetch the image data
+      const response = await axios.get(`${Local_Url}/api/v1/retailer/ackSlip/${id}`, {
+        responseType: 'blob',
+        headers: {
+          Accept: 'image/jpeg',
+        },
+      });
+
+      // Create a URL for the blob data
+      const imageUrl = URL.createObjectURL(response.data);
+
+      // Open the image URL in a new tab
+      window.open(imageUrl);
+    } catch (error) {
+      console.error('Error fetching image data:', error);
+    }
+  };
+
   const handleDownload = () => {
     // Implement download logic here
     window.open(fileUrl, "_blank");
@@ -17,7 +44,7 @@ function Slip({ fileUrl }) {
       </div>
       <div>
         <div className="AckSlip">
-          <button className="Ackslip button p-3 whitespace-nowrap" onClick={handleDownload}>
+          <button className="Ackslip button p-3 whitespace-nowrap"  onClick={handleClick}>
             Ack Slip
           </button>
         </div>
