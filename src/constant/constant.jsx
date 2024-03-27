366816821379d053fe250ea7671dca82160a96d8
Ritik kumar <ritikkumarkashyap9@gmail.com>
2024-03-27 14:28:30 +0530

    deployment 27032024

--- src/constant/constant.jsx
+++ src/constant/constant.jsx
@@ -1,4 +1,6 @@
-export const Local_Url = "http://localhost:5000";
+//export const Local_Url = "http://localhost:5000";
 
 //Development Url
 //export const Base_Url = "http://65.0.125.57:5000";
+
+export const Local_Url = "https://aadharupdates.com";
