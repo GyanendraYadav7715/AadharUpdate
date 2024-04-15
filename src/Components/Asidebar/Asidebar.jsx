f9a6adf3206a84695c0fcc456c91f9012543474e
Ritik kumar <ritikkumarkashyap9@gmail.com>
2024-04-15 19:39:21 +0530

    asidbar

--- src/Components/Asidebar/Asidebar.jsx
+++ src/Components/Asidebar/Asidebar.jsx
@@ -3,7 +3,7 @@ import Menudrop from "./Menudrop";
 import MenuItems from "./MenuItems";
 import "remixicon/fonts/remixicon.css";
 import "./Asidebar.css";
-import Demand from "../AsideUserDot/Demand";
+import Demand from "../AsideUserDot/Demand"
 
 const Asidebar = () => {
   const navigate = useNavigate();
@@ -12,35 +12,33 @@ const Asidebar = () => {
     
     localStorage.clear();
     navigate("/");
-  
   };
 
   return (
     <>
       <aside
         id="separator-sidebar"
-        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full  mt-14 sm:translate-x-0 shadow-md"
+        className="fixed top-0 left-0 z-40 w-64 h-[100vh] transition-transform -translate-x-full  mt-11 sm:translate-x-0 shadow-md"
         aria-label="Sidebar"
       >
         <div className="h-full py-4 shadow-2xl" id="tomakescrolldisable">
-<Demand/>
+          <Demand />
           <MenuItems />
 
-          <ul className="px-2 py-2 mt-4 space-y-2 font-medium border-t border-gray-400">
+          <ul className="px-2 py-1 space-y-2 font-medium">
             <Menudrop />
-          </ul>
-
-          <ul className="px-2 mt-4 space-y-2 font-medium border-t border-gray-400 py-2">
             <li className="hover:bg-gray-200 rounded-lg">
               <button
                 onClick={handlelogout}
-                className="flex items-center p-2 text-black   no-underline"
+                className="flex items-center px-2 py-1.5 text-black   no-underline"
               >
                 <i className="ri-shut-down-fill w-5 h-5 text-black transition duration-75 group-hover:text-gray-900"></i>
                 <span className="ms-3">Log-Out</span>
               </button>
             </li>
           </ul>
+
+          <ul className="px-2 mt-4 space-y-2 font-medium border-t border-gray-400 py-2"></ul>
         </div>
       </aside>
     </>
